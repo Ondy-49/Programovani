17c2e2eb4eae7c8081d96612f79732f3c81dd739
Drews <ondrahak2006@seznam.cz>
2025-04-09 20:38:38 +0200

    Malovani added

---
+++ MALOVANI/Malovani/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.colorPicker = new System.Windows.Forms.Button();
+            this.sizeButton = new System.Windows.Forms.Button();
+            this.sizeInput = new System.Windows.Forms.TextBox();
+            this.ellipseButton = new System.Windows.Forms.Button();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxFill = new System.Windows.Forms.CheckBox();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 443);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(574, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // colorPicker
+            // 
+            this.colorPicker.Location = new System.Drawing.Point(574, 33);
+            this.colorPicker.Name = "colorPicker";
+            this.colorPicker.Size = new System.Drawing.Size(75, 23);
+            this.colorPicker.TabIndex = 2;
+            this.colorPicker.Text = "Color Picker";
+            this.colorPicker.UseVisualStyleBackColor = true;
+            this.colorPicker.Click += new System.EventHandler(this.colorPicker_Click);
+            // 
+            // sizeButton
+            // 
+            this.sizeButton.Location = new System.Drawing.Point(574, 62);
+            this.sizeButton.Name = "sizeButton";
+            this.sizeButton.Size = new System.Drawing.Size(75, 23);
+            this.sizeButton.TabIndex = 3;
+            this.sizeButton.Text = "Set Size:";
+            this.sizeButton.UseVisualStyleBackColor = true;
+            this.sizeButton.Click += new System.EventHandler(this.sizeButton_Click);
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.Location = new System.Drawing.Point(655, 64);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(100, 20);
+            this.sizeInput.TabIndex = 4;
+            // 
+            // ellipseButton
+            // 
+            this.ellipseButton.Location = new System.Drawing.Point(574, 133);
+            this.ellipseButton.Name = "ellipseButton";
+            this.ellipseButton.Size = new System.Drawing.Size(75, 23);
+            this.ellipseButton.TabIndex = 5;
+            this.ellipseButton.Text = "Ellipse";
+            this.ellipseButton.UseVisualStyleBackColor = true;
+            this.ellipseButton.Click += new System.EventHandler(this.ellipseButton_Click);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(655, 135);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 6;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(655, 161);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(761, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(761, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Height";
+            // 
+            // checkBoxFill
+            // 
+            this.checkBoxFill.AutoSize = true;
+            this.checkBoxFill.Location = new System.Drawing.Point(655, 187);
+            this.checkBoxFill.Name = "checkBoxFill";
+            this.checkBoxFill.Size = new System.Drawing.Size(38, 17);
+            this.checkBoxFill.TabIndex = 10;
+            this.checkBoxFill.Text = "Fill";
+            this.checkBoxFill.UseVisualStyleBackColor = true;
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.Location = new System.Drawing.Point(574, 162);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(75, 23);
+            this.buttonRectangle.TabIndex = 11;
+            this.buttonRectangle.Text = "Rectangle";
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonImage
+            // 
+            this.buttonImage.Location = new System.Drawing.Point(574, 242);
+            this.buttonImage.Name = "buttonImage";
+            this.buttonImage.Size = new System.Drawing.Size(75, 23);
+            this.buttonImage.TabIndex = 12;
+            this.buttonImage.Text = "Load Image";
+            this.buttonImage.UseVisualStyleBackColor = true;
+            this.buttonImage.Click += new System.EventHandler(this.buttonImage_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonImage);
+            this.Controls.Add(this.buttonRectangle);
+            this.Controls.Add(this.checkBoxFill);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.ellipseButton);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.sizeButton);
+            this.Controls.Add(this.colorPicker);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button colorPicker;
+        private System.Windows.Forms.Button sizeButton;
+        private System.Windows.Forms.TextBox sizeInput;
+        private System.Windows.Forms.Button ellipseButton;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxFill;
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button buttonImage;
+    }
+}
+
---
+++ MALOVANI/Malovani/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;                     //promenne pro praci s inputy od uzivatele
+        private Point lastPoint = Point.Empty;
+        private Pen pen = new Pen(Color.Black, 2);
+        private Bitmap canvas;
+        private bool readyToDrawEllipse = false;
+        private int ellipseWidth = 0;
+        private int ellipseHeight = 0;
+        private bool readyToDrawRectangle = false;
+        private int rectangleWidth = 0;
+        private int rectangleHeight = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = canvas;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            pictureBox1.MouseClick += pictureBox1_MouseClick;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            lastPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)  //chatGPT
+            {
+                using (Graphics g = Graphics.FromImage(canvas))
+                {
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+
+                    float dx = e.X - lastPoint.X;
+                    float dy = e.Y - lastPoint.Y;
+                    float distance = (float)Math.Sqrt(dx * dx + dy * dy);
+
+
+                    if (distance > 0)
+                    {
+                        for (float i = 0; i <= 1; i += 1 / distance)
+                        {
+                            float x = lastPoint.X + (dx * i);
+                            float y = lastPoint.Y + (dy * i);
+                            g.FillEllipse(new SolidBrush(pen.Color), x - pen.Width / 2, y - pen.Width / 2, pen.Width, pen.Width);
+                        }
+                    }
+                }
+                pictureBox1.Invalidate();
+                lastPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+            lastPoint = Point.Empty;
+        }
+
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = canvas;
+        }
+
+        private void colorPicker_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = cd.Color;
+            }
+        }
+
+        private void sizeButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(sizeInput.Text, out int newSize) && newSize > 0)
+            {
+                pen.Width = newSize;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number greater than 0.");
+            }
+        }
+
+        private void ellipseButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxHeight.Text, out int height) && height > 0 && int.TryParse(textBoxWidth.Text, out int width) && width > 0)
+            {
+                ellipseWidth = width;
+                ellipseHeight = height;
+                readyToDrawEllipse = true;
+                MessageBox.Show("Click where you want the center to be.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid arguments greater than 0.");
+            }
+        }
+
+        private void buttonRectangle_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxHeight.Text, out int height) && height > 0 && int.TryParse(textBoxWidth.Text, out int width) && width > 0)
+            {
+                rectangleWidth = width;
+                rectangleHeight = height;
+                readyToDrawRectangle = true;
+                MessageBox.Show("Click where you want the center to be.");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid arguments greater than 0.");
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (readyToDrawEllipse)
+            {
+                if (checkBoxFill.Checked) //s vyplni
+                {
+                    using (Graphics g = Graphics.FromImage(canvas))
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        using (SolidBrush brush = new SolidBrush(pen.Color))
+                            g.FillEllipse(brush, e.X - ellipseWidth / 2, e.Y - ellipseHeight / 2, ellipseWidth, ellipseHeight);
+                    }
+                }
+                else
+                    using (Graphics g = Graphics.FromImage(canvas)) //bez vyplne
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        g.DrawEllipse(pen, e.X - ellipseWidth / 2, e.Y - ellipseHeight / 2, ellipseWidth, ellipseHeight);
+                    }
+
+                pictureBox1.Invalidate();
+                readyToDrawEllipse = false;
+            }
+            if (readyToDrawRectangle) 
+            {
+                if (checkBoxFill.Checked) //s vyplni
+                {
+                    using (Graphics g = Graphics.FromImage(canvas))
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        using (SolidBrush brush = new SolidBrush(pen.Color))
+                            g.FillRectangle(brush, e.X - rectangleWidth / 2, e.Y - rectangleHeight / 2, rectangleWidth, rectangleHeight);
+                    }
+                }
+                using (Graphics g = Graphics.FromImage(canvas)) //bez vyplne
+                {
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    g.DrawRectangle(pen, e.X - rectangleWidth / 2, e.Y - rectangleHeight / 2, rectangleWidth, rectangleHeight);
+                }
+
+                pictureBox1.Invalidate();
+                readyToDrawRectangle = false;
+            }
+        }
+
+        private void buttonImage_Click_1(object sender, EventArgs e)
+        {
+            int width, height;
+            if (int.TryParse(textBoxWidth.Text, out width) && int.TryParse(textBoxHeight.Text, out height))
+            {
+                Image masterOfTheMovement = Properties.Resources.masterOfTheMovement;
+                Image resized = new Bitmap(masterOfTheMovement, new Size(width, height));
+                canvas = new Bitmap(resized); //tvorba noveho platna ve velikosti obrazku s pozadi obrazku, abych pres to mohl kreslit
+                pictureBox1.Image = canvas;
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid arguments.");
+            }
+        }
+    }
+}
+
+
+
